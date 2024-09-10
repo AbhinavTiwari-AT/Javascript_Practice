7174b6423bcb0a5d563b0c829b626ddf2e308c41
Abhinav Tiwari <113198133+AbhinavTiwari-AT@users.noreply.github.com>
2024-09-10 20:23:10 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,24 @@
+//alert("3 + 3")  //  we are using node js not browser
+
+console.log(3+3) 
+// code readability should be high
+console.log("abhinav")
+
+
+let name = "abhinav"
+let age = 18
+let isLoggedIn =false
+
+//number  => 2 to power 53
+// bigint 
+// string =>" "
+//boolean => true/false
+//null => standalone value
+//undefined =>
+// symbol => uniques
+
+//object 
+
+console.log(typeof "abhinav");
+
+console.log(typeof age);
