b73fbacefcd3a6fbe2913de80f3b914009d34ee1
Abhinav Tiwari <113198133+AbhinavTiwari-AT@users.noreply.github.com>
2024-09-10 20:22:34 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,25 @@
+const accountId = 14453
+let accountEmail = "abhinav4903@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState;
+
+// accountId=2
+
+accountEmail = "hc@.com"
+accountPassword =" 23232"
+accountCity= "Delhi"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState
+])
+
+
+/*
+prefer not to use var
+because of issue in block scope and function scope
+
+always use const and let 
+
+*/
